96499bdc8a5081f104a0523104ed6f689ba35c51
ibrahimmbyrrm <ibrahim_bayram22@yahoo.com>
2023-03-06 18:55:07 +0300

    Initial Commit

---
+++ RickAndMortyCharacters/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickAndMortyCharacters
+//
+//  Created by İbrahim Bayram on 6.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
